00e93a92f9dda6dc0e5a3bc162685df1a0e02bc1
Hayden Lugo <hayden@lugowebdevelopment.com>
2021-09-10 10:47:31 -0500

    Final I think

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,72 @@
 var time = moment();
+var timeElement = $(".time-block")
 $("#today").text(`${time.format("dddd, MMMM Do YYYY")}`);
 $("#today-time").text(`${time.format('hh:mm a')}`);
 
+
+
 $(".saveBtn").on("click", function() {
-    var timeBlock = $(this).parent().attr("id");
-    var plannerText = $(this).siblings(".description").val();
-    localStorage.setItem(timeBlock, plannerText);
-    console.log(`Time: ${timeBlock}  |  Plans: ${plannerText}`);
+    var timeElem = $(this).parent().attr("class").split(" ")[2];
+    var eventText = $(this).siblings(".description").val();
+    var availibility = {
+        time: timeElem,
+        event: eventText
+    }
+    if (typeof window.localStorage.savedEvent === "string") {
+        var tempArray = JSON.parse(window.localStorage.getItem("savedEvent"));
+    } else {
+        var tempArray = [];
+    }
+    tempArray.push(availibility);
+    window.localStorage.setItem("savedEvent", JSON.stringify(tempArray));
 });
+
+
+
 saveScheduler = () => {
+    var tempArray = JSON.parse(window.localStorage.getItem("savedEvent"));
     $(".time-block").each(function() {
-        var id = $(this).attr("id");
-        var text = localStorage.getItem(id);
-        if (text !== null) {
-            $(this).children(".description").val(text);
+        if (typeof window.localStorage.savedEvent === "string") {
+            var tempArray = JSON.parse(window.localStorage.getItem("savedEvent"));
+            $(`#${tempArray[0].time}`).siblings(".description").innerHTML = tempArray[0].event;
         }
     })
-    
+    const pushLocation = (obj) => {
+        var flag = 0;
+        for (let i=0; i<tempArray.length; i++) {
+            if (tempArray[i].time == obj.classList[2]) {
+                flag = 1;
+                obj.children[1].innerHTML = tempArray[i].event;
+            }
+        }
+        if (flag === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    for (let i=0; i<timeElement.length; i++) {
+        pushLocation(timeElement[i]);
+    }
 };
 
-saveScheduler();
+
 
 setColorTimer = () => {
     var currentHour = time.toObject().hours;
     $(".time-block").each(function() {
         var thisHour = parseInt($(this).attr("id"));
         if (thisHour < currentHour) {
-            $(this).addClass("past")
+            $(this).css("background-color", "hsla(0, 0%, 83%, 0.8)")
         } else if (thisHour === currentHour) {
-            $(this).addClass("present")
+            $(this).css("background-color", "hsla(5, 95%, 43%, 0.8)")
         } else {
-            $(this).addClass("future")
+            $(this).css("background-color", "hsla(151, 78%, 49%, 0.8)")
         };
     })
 };
 
+
+
 setColorTimer();
+saveScheduler();
